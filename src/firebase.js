ada99f1762abb68978acfaf19996772fe1a5f11a
Mikael Koivula <mikaelkoivula@Mikaels-MacBook-Pro.local>
2020-09-25 15:52:41 +0300

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAmH8XTCzWsRxbTpIWFyViZnVGQVSQCZWc",
+	authDomain: "fb-clone-54fe0.firebaseapp.com",
+	databaseURL: "https://fb-clone-54fe0.firebaseio.com",
+	projectId: "fb-clone-54fe0",
+	storageBucket: "fb-clone-54fe0.appspot.com",
+	messagingSenderId: "633421381663",
+	appId: "1:633421381663:web:cafa5e9cd6a295703b552a",
+	measurementId: "G-K12WE3Y0H3"
+  };
